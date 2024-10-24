b1f0a064097a18a7390919245beb9ddc45057e80
icecrao2 <icecrao2@daum.net>
2024-10-24 14:46:18 +0900

    change

---
+++ lib/ruler_picker/element/moon_ruler_drawer.dart
@@ -0,0 +1,70 @@
+
+
+part of ruler_picker_lib;
+
+
+
+class _MoonRulerDrawer {
+
+  final double selectedNumber;
+  final Color color;
+  final Color selectedColor;
+  final double strokeWidth;
+  final double rulerPickerHeight;
+  final Size size;
+  final double lineBetweenAlignWidth;
+  final double longVerticalLineHeightRatio;
+  final double shortVerticalLineHeightRatio;
+  final int maxNumber;
+  final int minNumber;
+  final double pickedVerticalLineHeightRatio;
+  final TextStyle textStyle;
+  final TextPainter textPainter;
+
+  _MoonRulerDrawer({
+    required this.selectedNumber,
+    required this.color,
+    required this.selectedColor,
+    required this.strokeWidth,
+    required this.rulerPickerHeight,
+    required this.size,
+    required this.lineBetweenAlignWidth,
+    required this.longVerticalLineHeightRatio,
+    required this.shortVerticalLineHeightRatio,
+    required this.maxNumber,
+    required this.minNumber,
+    required this.pickedVerticalLineHeightRatio,
+    required this.textStyle,
+    required this.textPainter
+  });
+
+  void draw(PaintingContext context, Offset offset) { }
+
+  _MoonRulerDrawer getPickedLine(_MoonRulerPickedLinesType type) {
+
+    switch(type) {
+      case _MoonRulerPickedLinesType.defaultLine:
+        return _PickedRulerLine(selectedNumber: selectedNumber, color: color, selectedColor: selectedColor, strokeWidth: strokeWidth, rulerPickerHeight: rulerPickerHeight, size: size, lineBetweenAlignWidth: lineBetweenAlignWidth, longVerticalLineHeightRatio: longVerticalLineHeightRatio, shortVerticalLineHeightRatio: shortVerticalLineHeightRatio, maxNumber: maxNumber, minNumber: minNumber, pickedVerticalLineHeightRatio: pickedVerticalLineHeightRatio, textStyle: textStyle, textPainter: textPainter);
+    }
+  }
+
+  _MoonRulerDrawer getLines(_MoonRulerLinesType type) {
+
+    switch(type) {
+      case _MoonRulerLinesType.defaultLine:
+        return _RulerLines(selectedNumber: selectedNumber, color: color, selectedColor: selectedColor, strokeWidth: strokeWidth, rulerPickerHeight: rulerPickerHeight, size: size, lineBetweenAlignWidth: lineBetweenAlignWidth, longVerticalLineHeightRatio: longVerticalLineHeightRatio, shortVerticalLineHeightRatio: shortVerticalLineHeightRatio, maxNumber: maxNumber, minNumber: minNumber, pickedVerticalLineHeightRatio: pickedVerticalLineHeightRatio, textStyle: textStyle, textPainter: textPainter);
+      case _MoonRulerLinesType.lineWithLabel:
+        return _RulerLinesWithLabel(selectedNumber: selectedNumber, color: color, selectedColor: selectedColor, strokeWidth: strokeWidth, rulerPickerHeight: rulerPickerHeight, size: size, lineBetweenAlignWidth: lineBetweenAlignWidth, longVerticalLineHeightRatio: longVerticalLineHeightRatio, shortVerticalLineHeightRatio: shortVerticalLineHeightRatio, maxNumber: maxNumber, minNumber: minNumber, pickedVerticalLineHeightRatio: pickedVerticalLineHeightRatio, textStyle: textStyle, textPainter: textPainter);
+    }
+  }
+
+}
+
+
+enum _MoonRulerPickedLinesType {
+  defaultLine,
+}
+
+enum _MoonRulerLinesType {
+  defaultLine, lineWithLabel
+}
--- lib/ruler_picker_lib.dart
+++ lib/ruler_picker_lib.dart
@@ -3,12 +3,14 @@
 library ruler_picker_lib;
 
 import 'dart:async';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 part 'ruler_picker/ruler_picker.dart';
-part 'ruler_picker/element/ruler_vertical_line.dart';
-part 'ruler_picker/element/ruler_aligned_long_vertical_line.dart';
-part 'ruler_picker/element/ruler_aligned_short_vertical_line.dart';
-part 'ruler_picker/element/vertical_line.dart';
 
+part 'ruler_picker/element/moon_ruler_drawer.dart';
+part 'ruler_picker/element/picked_ruler_line.dart';
+
+part 'ruler_picker/element/ruler_lines.dart';
+part 'ruler_picker/element/ruler_lines_with_label.dart';
